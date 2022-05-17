8518663ecf301fe38bd4ba23ce109d192053eba5
Chrezm <TheChrezm@gmail.com>
2022-05-16 21:29:46 -0400

    Bump

--- server/tsuserver.py
+++ server/tsuserver.py
@@ -70,8 +70,8 @@ class TsuserverDR:
         self.release = 4
         self.major_version = 3
         self.minor_version = 1
-        self.segment_version = 'x1'
-        self.internal_version = '220505a'
+        self.segment_version = 'post1'
+        self.internal_version = '220516a'
         version_string = self.get_version_string()
         self.software = 'TsuserverDR {}'.format(version_string)
         self.version = 'TsuserverDR {} ({})'.format(version_string, self.internal_version)
