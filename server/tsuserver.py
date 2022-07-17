25bbb1d118b7640fab99e5b5a8ab15f6ef03e141
Chrezm <TheChrezm@gmail.com>
2022-07-17 06:31:59 -0400

    4.3.3

--- server/tsuserver.py
+++ server/tsuserver.py
@@ -69,8 +69,8 @@ class TsuserverDR:
         self.release = 4
         self.major_version = 3
         self.minor_version = 3
-        self.segment_version = 'RC1'
-        self.internal_version = 'm220716b'
+        self.segment_version = ''
+        self.internal_version = '220717a'
         version_string = self.get_version_string()
         self.software = 'TsuserverDR {}'.format(version_string)
         self.version = 'TsuserverDR {} ({})'.format(version_string, self.internal_version)
