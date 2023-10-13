58b89fd0139ee0a3a4255061b5c4514b144c81d5
Keightiie <35301742+Keightiie@users.noreply.github.com>
2023-10-13 23:31:34 +1000

    Bump tsuserver to release version

--- server/tsuserver.py
+++ server/tsuserver.py
@@ -69,8 +69,8 @@ class TsuserverDR:
         self.release = 5
         self.major_version = 2
         self.minor_version = 0
-        self.segment_version = 'beta2'
-        self.internal_version = '230715a'
+        self.segment_version = 'release'
+        self.internal_version = '231013a'
         version_string = self.get_version_string()
         self.software = 'TsuserverDR {}'.format(version_string)
         self.version = 'TsuserverDR {} ({})'.format(version_string, self.internal_version)
