fe27f30305180c40cb03ee3081f3f8271eb30ae2
Keightiie <35301742+Keightiie@users.noreply.github.com>
2025-04-23 00:04:58 +1000

    Bump up version number

--- server/tsuserver.py
+++ server/tsuserver.py
@@ -68,9 +68,9 @@ class TsuserverDR:
 
         self.release = 5
         self.major_version = 4
-        self.minor_version = 0
+        self.minor_version = 1
         self.segment_version = ''
-        self.internal_version = '240321a'
+        self.internal_version = '250423a'
         version_string = self.get_version_string()
         self.software = 'TsuserverDR {}'.format(version_string)
         self.version = 'TsuserverDR {} ({})'.format(version_string, self.internal_version)
